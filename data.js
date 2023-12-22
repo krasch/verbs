ae3970d6b57bebc8f3eb7c5b2872d8e8c2e72c88
krasch <dev@krasch.io>
2023-12-22 15:58:57 +0100

    Change type of challenge

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
-/* utilities */ 
+/***************************************
+ *   utilities
+ ***************************************/
 
 const objectToMap = obj => new Map(Object.entries(obj));
 
@@ -10,50 +12,59 @@ Map.prototype.random = function () {
   return Array.from(this.keys()).random();
 }
 
+const isVowel = char => /[aeiouéè]/i.test(char);
 
-/* data */
+/***************************************
+ *   data
+ ***************************************/
 
 const verbs = objectToMap(verbs_);
 
-
-const verbForms = objectToMap({
-    0: "present",
-    1: "imperfect",
-    2: "future",
-    3: "conditional"
-})
-
-const verbFormsTranslation = objectToMap({
-    "present": "présent",
-    "imperfect": "imparfait",
-    "future": "futur simple",
-    "conditional": "conditional présent"
-})
-
 const persons = objectToMap({
-    "first person singular": ["je"],
-    "second person singular": ["tu"],
-    "third person singular": ["elle", "il", "on"],
-    "first person plural": ["nous"],
-    "second person plural": ["vous"],
-    "third person plural": ["elles", "ils"]
+    0: ["je"],
+    1: ["tu"],
+    2: ["elle", "il", "on"],
+    3: ["nous"],
+    4: ["vous"],
+    5: ["elles", "ils"]
+});
+
+const personsInverse = objectToMap({
+    "je": 0,
+    "tu": 1,
+    "elle": 2,
+    "il": 2,
+    "on": 2,
+    "nous": 3,
+    "vous": 4,
+    "elles": 5,
+    "ils": 6
 })
 
-/* functions */
 
-function getChallenge() {
-    const verb = verbs.random();
-    const person = persons.random();
-    const pronoun = persons.get(person).random();
-    const form = verbForms.random();
-    
-    const key = "indicative"+"|"+verbForms.get(form)+"|"+person;
-    const conjugated = verbs.get(verb)[key];
-    let phrase = pronoun + " " + conjugated;
-    if ((pronoun == "je") && (conjugated.startsWith("a"))) // e, i, o, or u todo
-       phrase = "j'" + conjugated;
-    
-    
-    return {"infinitive": verb, "conjugated": phrase, "answer": form};
+/***************************************
+ *   functions
+ ***************************************/
+
+function prepareForDisplay(person, conjugated){
+    if ((person === "je") && (isVowel(conjugated[0])))
+        return "j'"+conjugated;
+    else
+        return person + " " + conjugated
 }
+
+function getRandomInfinitive() {
+    return verbs.random()
+}
+
+function getRandomPerson() {
+    return persons.get(persons.random()).random();
+}
+
+function getConjugation(infinitive, person){
+    const personIndex = personsInverse.get(person);
+    const conjugated = verbs.get(infinitive)[personIndex];
+    return prepareForDisplay(person, conjugated);
+}
+
 
